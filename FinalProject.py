adef1956977819bdb393a0879cbb5f89a9806b0d
sophianarido <sophiadazed@gmail.com>
2024-05-17 22:05:01 -0400

    Calculations of spendings
    Determines the monthly spending and average daily spending through a mathematical operation.
    Usage of if else statements in order to calculate the monthly budget

--- FinalProject.py
+++ FinalProject.py
@@ -10,8 +10,20 @@ def calculate_monthly_budget():
             try:
                 spending = float(input(f"Enter how much you spent for day {day}: $"))
                 if spending < 0:
-                    print("Please enter a positive number.")
+                    print("Please enter a number.")
                 else: daily_spendings.append(spending)
                 break
             except ValueError:
-                print("Invalid input. Please eneter a number.")
+                print("Invalid input. Please eneter a positive number.")
+#Calculate total spending for the month 
+total_spending = sum(daily_spendings)
+
+#Calculate average daily spending
+average_daily_spending = total_spending / total_days
+
+#Calculate monthly budget
+monthly_budget = 0
+if total_spending < 0:
+    monthly_budget = "Invalid input. Totaly spending cannot be negative."
+else: 
+    monthly_budget = total_spending - (average_daily_spending * total_days)
